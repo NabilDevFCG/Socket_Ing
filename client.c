3ac84f426d1f4340b77e1359f8cfc8d6a58d1f34
NabilDev <saadinabildev@gmail.com>
2023-08-19 02:23:00 +0200

    add counter to server message to count number of sent message

--- client.c
+++ client.c
@@ -21,8 +21,16 @@ int main(int argc, char const* argv[])
 	serv_addr.sin_port = htons(PORT);
 
 
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.5", &serv_addr.sin_addr) <= 0) 
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
 	/*socket descriptor*/
-	client_fd = socket(AF_INET, SOCK_STREAM, 0)
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_fd < 0) 
 	{
 		printf("\n Socket creation error \n");
@@ -30,17 +38,11 @@ int main(int argc, char const* argv[])
 	}
 	
 
-	// Convert IPv4 and IPv6 addresses from text to binary
-	// form
-	if (inet_pton(AF_INET, "192.168.1.31", &serv_addr.sin_addr) <= 0) 
-	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
-	}
+	
 
 	/*Connect socket to address*/
-	status =  connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)
-	if (status) < 0) 
+	status =  connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if (status < 0) 
 	{
 		printf("\nConnection Failed \n");
 		return -1;
